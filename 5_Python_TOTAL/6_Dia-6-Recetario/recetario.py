705e836a4a87cf9e9a09566d6f4092f465b8ce53
Gaby <ggabrieldeveloper@gmail.com>
2025-08-07 20:05:14 -0300

    add: opción 1

---
+++ 5_Python_TOTAL/6_Dia-6-Recetario/recetario.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+import os
+import platform
+
+base_path = Path(__file__).parent / "Recetas"
+
+def leer_receta(categoria, receta):
+    receta_path = base_path / categoria / receta
+
+    if receta_path.exists():
+        print(receta_path.read_text(encoding='utf-8'))
+    else:
+        print("La receta no existe.")
+
+
+
+def contar_recetas():
+    contador_recetas = 0
+    for file in base_path.rglob("*.txt"):
+        contador_recetas += 1
+
+    return contador_recetas
+
+def limpiar_consola():
+    os.system("cls" if platform.system() == "Windows" else "clear")
+
+def obtener_recetas(categoria):
+    categoria_path = base_path / categoria
+    if categoria_path.exists():
+        return [receta.name for receta in categoria_path.glob("*.txt")]
+
+    return []
+
+def mostrar_categorias():
+    return [categoria.name for categoria in base_path.iterdir() if categoria.is_dir()]
+
+def seleccionar_categoria():
+    print("Categorías disponibles: ")
+    categorias = mostrar_categorias()
+
+    for categoria in categorias:
+        print(f'- {categoria}')
+
+    categoria = input("Selecciona una categoria: ").strip()
+    recetas = obtener_recetas(categoria)
+    if recetas:
+        print("Recetas disponibles:")
+        for receta in recetas:
+            print(f'- {receta}')
+        receta = input("¿Qué receta deseas leer? ").strip()
+        leer_receta(categoria, receta)
+
+    else:
+        print("La categoría no existe")
+
+
+
+def mostrar_menu():
+    print("¿Qué deseas hacer?")
+    print("[1] Leer una receta")
+    print("[2] Crear una receta")
+    print("[3] Crear una categoría")
+    print("[4] Eliminar una receta")
+    print("[5] Eliminar una categoría")
+    print("[6] Salir")
+
+def ejecutar():
+    print("¡Bienvenido al gestor de recetas!")
+    print(f"Hay un total de {contar_recetas()} recetas.")
+
+    while True:
+        mostrar_menu()
+        opcion = input("Ingresar una opcion: ")
+
+        if opcion == "1":
+            seleccionar_categoria()
+
+        elif opcion == "2":
+            pass
+        elif opcion == "3":
+            pass
+        elif opcion == "4":
+            pass
+        elif opcion == "5":
+            pass
+        elif opcion == "6":
+            print("\nFinalizando gestor de recetas...")
+            break
+
+        else:
+            print("Opción no válida, intente nuevamente...")
+
+# INICIAR PROGRAMA
+ejecutar()
